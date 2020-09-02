6456c5b59bc5a90e210f587570d88e6e7c50acd4
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2020-09-02 12:51:19 -0500

    fix: change webpack setup to make it work with electron-builder

--- webpack.electron.config.js
+++ webpack.electron.config.js
@@ -1,25 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
-  devtool: 'source-map',
-  entry: './electron/main.ts',
-  target: 'electron-main',
+  devtool: "source-map",
+  entry: "./electron/main.ts",
+  target: "electron-main",
   module: {
     rules: [
       {
         test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
     ],
   },
+  node: {
+    __dirname: false,
+  },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: '[name].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
   },
 };
--- webpack.react.config.js
+++ webpack.react.config.js
@@ -1,39 +1,36 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    mainFields: ['main', 'module', 'browser'],
+    extensions: [".tsx", ".ts", ".js"],
+    mainFields: ["main", "module", "browser"],
   },
-  entry: './src/app.tsx',
-  target: 'electron-renderer',
-  devtool: 'source-map',
+  entry: "./src/app.tsx",
+  target: "electron-renderer",
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
         },
       },
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, '../dist/renderer'),
+    contentBase: path.join(__dirname, "../dist/renderer"),
     historyApiFallback: true,
     compress: true,
     hot: true,
     port: 4000,
-    publicPath: '/',
+    publicPath: "/",
   },
   output: {
-    path: path.resolve(__dirname, '../dist/renderer'),
-    filename: 'js/[name].js',
-    publicPath: './',
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js",
   },
-  plugins: [
-    new HtmlWebpackPlugin(),
-  ],
+  plugins: [new HtmlWebpackPlugin()],
 };
